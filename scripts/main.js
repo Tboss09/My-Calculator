c29bfc41099bc3b9430def65e778f47eadd66b97
Akosile Tayo <akosilety9@gmail.com>
2021-01-10 15:55:24 +0100

    Created some pages and started editing js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
 // First Define all the Element i am gonna use
 
 const numbers = document.querySelectorAll('[data-number]'); //Select all numbers
+const symbols = document.querySelectorAll('[data-symbol]'); //Select all symbols
+const equal = document.querySelector('[data-equal]'); //Select equal to button
+
+
+function inputValueWhenPressed() {
+    numbers.forEach(number => {
+        number.addEventListener("click", inputNumberValue);
+    })
+    function inputNumberValue() {
+        console.log(this.innerHTML);
+    }
+    return;
+}
+inputValueWhenPressed() 
