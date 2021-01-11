de8416c8c637e6c8f4fa69b4668b828c5f62b01f
Akosile Tayo <akosilety9@gmail.com>
2021-01-11 23:28:27 +0100

    Done

--- scripts/main.js
+++ scripts/main.js
@@ -1,61 +1,115 @@
+const keys = document.querySelector('.keyboard');
+const calculator = document.querySelector(".screen");
+const display = document.querySelector('.input p')
+// const result = document.querySelector('.results p')
+var item = [];
+// NOTE: 
+// This is the final source code file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-2
 
-const display = document.querySelector('.input p');
-const result = document.querySelector('.results p');
-const storeValue = document.querySelector('.hiddenDiv');
-const keyboard = document.querySelector('.keyboard');
-
-keyboard.addEventListener('click', calculator)
-
-function calculator(e) {
-    const buttonClicked = e.target;
-    const displayNum = display.innerHTML;
-
-    if (buttonClicked.matches('button')) {
-        const number = buttonClicked.dataset;
-
-        if (number.number) //if any of the number button is clicked,this happens
-        {
-            if (displayNum === '0') {
-                display.innerHTML = buttonClicked.innerHTML;
-                return;
-            };
-            display.innerHTML = displayNum + buttonClicked.innerHTML;
-        };
-
-        if (number.clear) //if Ac button is clicked 
-        {
-            display.innerHTML = ``;
-        }
-
-        if (number.dot) {
-            display.innerHTML += `.`;
-        }
-
-
-
-        if (buttonClicked.dataset.symbol) {
-            const symbol = buttonClicked.dataset.symbol;
-            
-            if (symbol === 'plus'
-                || symbol === "subtract"
-                || symbol === "divide"
-                || symbol === "multiply") {
-                storeValue.dataset.firstValue = displayNum;
-                storeValue.dataset.symbol = symbol;
-            }
-
-            if (symbol === "equal") {
-                const firstValue = storeValue.dataset.firstValue;
-                const secondValue = displayNum;
-                const secondNumber = storeValue.dataset.symbol;
-                console.log(firstValue);
-                console.log(secondNumber);
-                console.log(secondValue);
-            }
-
-        };
-
+// # START EDITING YOUR JAVASCRIPT HERE
+// ===============
+const calculate = (n1, operator, n2) => {
+    let result = ''
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2)
     }
+
+    return result
 }
 
 
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+        if (!action) {
+            if (
+                displayedNum === '0' ||
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+            ) {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+            calculator.dataset.previousKeyType = 'number'
+        }
+
+        if (action === 'decimal') {
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.'
+            } else if (
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+            ) {
+                display.textContent = '0.'
+            }
+
+            calculator.dataset.previousKeyType = 'decimal'
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            if (
+                firstValue &&
+                operator &&
+                previousKeyType !== 'operator' &&
+                previousKeyType !== 'calculate'
+            ) {
+                const calcValue = calculate(firstValue, operator, secondValue)
+                display.textContent = calcValue
+                calculator.dataset.firstValue = calcValue
+            } else {
+                calculator.dataset.firstValue = displayedNum
+            }
+
+            key.classList.add('is-depressed')
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.operator = action
+        }
+
+        if (action === 'clear') {
+            calculator.dataset.firstValue = ''
+            calculator.dataset.modValue = ''
+            calculator.dataset.operator = ''
+            calculator.dataset.previousKeyType = ''
+            display.textContent = 0
+            calculator.dataset.previousKeyType = 'clear'
+        }
+
+        if (action === 'calculate') {
+            let firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            let secondValue = displayedNum
+
+            if (firstValue) {
+                if (previousKeyType === 'calculate') {
+                    firstValue = displayedNum
+                    secondValue = calculator.dataset.modValue
+                }
+
+                display.textContent = calculate(firstValue, operator, secondValue)
+            }
+
+            calculator.dataset.modValue = secondValue
+            calculator.dataset.previousKeyType = 'calculate'
+        }
+    }
+})
